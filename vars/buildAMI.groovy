cbb8ee44897301301a76857b408d78d372bf9a89
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-11 23:15:21 +0530

    Adding

--- vars/buildAMI.groovy
+++ vars/buildAMI.groovy
@@ -1,10 +1,8 @@
 def call() {
   def packer = new packer.PackerBuild()
-  def config_dir = 'intro-to-packer'
-  def config_file = 'cocktails.pkr.hcl'
-  sh packer.init(config_dir)
-  def reponse = sh packer.build(config_dir, config_file)
-
-  println 'Printing response'
-  println response
+    def config_dir = 'intro-to-packer'
+    def config_file = 'cocktails.pkr.hcl'
+    sh packer.init(config_dir)
+    def output = sh(script: packer.build(config_dir, config_file), returnStdout: true).trim()
+    echo "Output of asg.test(): ${output}"
   }
