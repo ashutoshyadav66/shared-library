18475c703dce4665a26a0a8cfa61b9d4a89f1591
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-11 22:15:48 +0530

    adding

--- vars/buildAMI.groovy
+++ vars/buildAMI.groovy
@@ -2,6 +2,6 @@ def call() {
   def packer = new packer.PackerBuild()
   def config_dir = 'intro-to-packer'
   def config_file = 'cocktails.pkr.hcl'
-  sh packer.init(config_file)
+  sh packer.init(config_dir)
   sh packer.build(config_dir, config_file)
   }
