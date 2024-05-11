6e6f7c1e3f79d83482a77661ab71e4bff997b8dc
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-11 23:13:12 +0530

    Adding

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,6 @@
-
 def call() {
-  def asg = new cloud.awscli.AutoScalingGroup('Ashutosh', 'pw')
-  sh asg.test()
-
+    def asg = new cloud.awscli.AutoScalingGroup('Ashutosh', 'pw')
+    def output = sh(script: asg.test(), returnStdout: true).trim()
+    echo "Output of asg.test(): ${output}"
 }
 
