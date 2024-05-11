68e7ae6870b91b14b879c8a04bb0d4d1f11ee206
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-11 21:06:26 +0530

    adding

--- src/packer/PackerBuild.groovy
+++ src/packer/PackerBuild.groovy
@@ -1,16 +1,16 @@
 package packer
 
 class PackerBuild {
-    String packerFile
-    class PackerBuild(packerFile) {
-        this.packerFile = packerFile
-      }
+  String packerFile
+    PackerBuild(packerFile) {
+      this.packerFile = packerFile
+    }
 
-    def packerInit() {
-         return "packer init ."
-      }
-    def build(){
-        packerInit()
-
-      }
+  def packerInit() {
+    return "packer init ."
   }
+  def build(){
+    packerInit()
+
+  }
+}
