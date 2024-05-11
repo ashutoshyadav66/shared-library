18a814483785c079714fb47f3ad1f1e486ecfc25
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-11 23:51:06 +0530

    Adding

--- vars/buildAMI.groovy
+++ vars/buildAMI.groovy
@@ -9,8 +9,6 @@ def call() {
     def output = sh(script: packer.build(config_dir, config_file), returnStdout: true).trim()
 
     if (output.contains('amazon-ebs.cocktails: AMIs were created:')) {
-      def amiId = output =~ /(?<=AMIs were created:\n\w+: )ami-\w+/
-
         def lines = output.split('\n')
         def lastLine = lines[-1] 
         println lastLine
