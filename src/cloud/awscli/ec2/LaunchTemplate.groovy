7739d49c7cd6c85aabe2a39dba9c796f5e56471e
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 09:10:46 +0530

    Adding

--- src/cloud/awscli/ec2/LaunchTemplate.groovy
+++ src/cloud/awscli/ec2/LaunchTemplate.groovy
@@ -1,12 +1,22 @@
 package cloud.awscli.ec2
 
 class LaunchTemplate {
-    def createVersion(String launchTemplateId, String amiId) {
-        return """
-            aws ec2 create-launch-template-version \\
-            --launch-template-id ${launchTemplateId} \\
-            --launch-template-data '{"ImageId": "${amiId}"}'
+  def createVersion(String launchTemplateId, String sourceVersion, String amiId) {
+    if (sourceVersion == null) {
+      return """
+        aws ec2 create-launch-template-version \\
+        --launch-template-id ${launchTemplateId} \\
+        --launch-template-data '{"ImageId": "${amiId}"}'
         """
+    } else {
+      return """
+        aws ec2 create-launch-template-version \\
+        --source-version ${sourceVersion} \\
+        --launch-template-id ${launchTemplateId} \\
+        --launch-template-data '{"ImageId": "${amiId}"}'
+        """
+
     }
+  }
 }
 
--- vars/deployImmutableInfra.groovy
+++ vars/deployImmutableInfra.groovy
@@ -1,5 +1,5 @@
 def call() {
     def launchTemplate = new cloud.awscli.ec2.LaunchTemplate()
-    sh launchTemplate.createVersion("lt-0be196a4ebe44a88b", "ami-0c1e8a55e8ea6420f")
+    sh launchTemplate.createVersion("lt-0be196a4ebe44a88b", "1", "ami-0c1e8a55e8ea6420f")
 
   }
