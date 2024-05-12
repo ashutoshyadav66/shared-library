2abcb5305f098ac49890d610f86e2c13d7396395
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 10:27:50 +0530

    Adding

--- vars/deployImmutableInfra.groovy
+++ vars/deployImmutableInfra.groovy
@@ -2,8 +2,8 @@ import groovy.json.JsonSlurperClassic
 def call() {
   def launchTemplate = new cloud.awscli.ec2.LaunchTemplate()
     def output = sh(script: launchTemplate.createVersion("lt-0be196a4ebe44a88b", "10", "ami-0c1e8a55e8ea6420f"), returnStdout: true).trim()
-    def jsonSlurper = new JsonSlurper()
-    def jsonObject = JsonSlurperClassic.parseText(output)
+    def jsonSlurper = new JsonSlurperClassic()
+    def jsonObject = jsonSlurper.parseText(output)
     def launchTemplateId = jsonObject.LaunchTemplateVersion.LaunchTemplateId
     def versionNumber = jsonObject.LaunchTemplateVersion.VersionNumber
     sh launchTemplate.modifyLaunchTemplate(launchTemplateId, versionNumber)
