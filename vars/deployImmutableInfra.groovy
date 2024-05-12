044ffc3590d9b5ce458a52ed5d45dbc45a4d86dc
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 09:46:00 +0530

    Adding

--- vars/deployImmutableInfra.groovy
+++ vars/deployImmutableInfra.groovy
@@ -4,5 +4,8 @@ def call() {
     def output = sh(script: launchTemplate.createVersion("lt-0be196a4ebe44a88b", "1", "ami-0c1e8a55e8ea6420f"), returnStdout: true).trim()
     def jsonSlurper = new JsonSlurper()
     def jsonObject = jsonSlurper.parseText(output)
+    def launchTemplateId = jsonObject.LaunchTemplateVersion.LaunchTemplateId
+    def versionNumber = jsonObject.LaunchTemplateVersion.VersionNumber
+    println versionNumber
 
 }
