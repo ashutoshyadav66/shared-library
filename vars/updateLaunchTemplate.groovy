2a29a6af4e3161241bc14c4111320f0566982678
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 12:53:17 +0530

    Adding

--- vars/updateLaunchTemplate.groovy
+++ vars/updateLaunchTemplate.groovy
@@ -4,16 +4,18 @@ def call(String launchTemplateId, String amiId) {
 
     def defaultVersion = readJSON text: sh(script: launchTemplate.getDefaultVersion(launchTemplateId), returnStdout: true).trim()
 
-    println "Default launch template version: ${defaultVersion[-1]}"
+    println "Retrieved default version of launch template: ${defaultVersion[-1]}"
 
     def output = sh(script: launchTemplate.createVersion(launchTemplateId, defaultVersion[-1], amiId), returnStdout: true).trim()
 
-    println "Created new version of ${defaultVersion[0]} launch template"
+    println "Created a new version of launch template ${defaultVersion[0]}"
 
     def jsonOutput = readJSON text: output
 
     def versionNumber = jsonOutput.LaunchTemplateVersion.VersionNumber
 
   sh launchTemplate.modifyLaunchTemplate(launchTemplateId, versionNumber)
-  println "Made default launch template ${defaultVersion[0]} to ${versionNumber} version"
+  println "Successfully set default version ${versionNumber} for launch template ${defaultVersion[0]}."
+
+  return launchTemplateId
 }
