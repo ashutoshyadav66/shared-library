2696ac4aa51b9d5d132fd371a53b717ca62eb4e4
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 10:33:21 +0530

    Adding

--- vars/deployImmutableInfra.groovy
+++ vars/deployImmutableInfra.groovy
@@ -1,7 +1,7 @@
 def call() {
   def launchTemplate = new cloud.awscli.ec2.LaunchTemplate()
     def output = sh(script: launchTemplate.createVersion("lt-0be196a4ebe44a88b", "10", "ami-0c1e8a55e8ea6420f"), returnStdout: true).trim()
-    def versionNumber = output['LaunchTemplateVersion']['VersionNumber']
-    println versionNumber
-    // sh launchTemplate.modifyLaunchTemplate(launchTemplateId, versionNumber)
+    def jsonOutput = readJSON text: output
+    def versionNumber = jsonOutput.LaunchTemplateVersion.VersionNumber
+    println(versionNumber)    // sh launchTemplate.modifyLaunchTemplate(launchTemplateId, versionNumber)
 }
