cb74bc115b21167ff0023f482f267b94b1513c9f
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 12:27:03 +0530

    Adding

--- vars/updateLaunchTemplate.groovy
+++ vars/updateLaunchTemplate.groovy
@@ -1,9 +1,16 @@
 def call(String launchTemplateId, String amiId) {
   def launchTemplate = new cloud.awscli.ec2.LaunchTemplate()
-    def defaultVersion = readJSON text: sh(script: launchTemplate.getDefaultVersion(launchTemplateId))
+
+    def readDefaultVersion = sh(script: launchTemplate.getDefaultVersion(launchTemplateId))
+    
+    def defaultVersion = readJSON text: readDefaultVersion
+
     println defaultVersion
     def output = sh(script: launchTemplate.createVersion(launchTemplateId, "10", amiId), returnStdout: true).trim()
+
     def jsonOutput = readJSON text: output
+
     def versionNumber = jsonOutput.LaunchTemplateVersion.VersionNumber
+
   sh launchTemplate.modifyLaunchTemplate(launchTemplateId, versionNumber)
 }
