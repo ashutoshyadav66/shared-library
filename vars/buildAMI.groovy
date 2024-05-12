49266fda8cad70191544f3e484b9fe4514051c3a
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 13:02:12 +0530

    Adding

--- vars/buildAMI.groovy
+++ vars/buildAMI.groovy
@@ -1,18 +1,22 @@
-def call() {
-  def packer = new packer.awsAMI()
+def call(String config_dir, String config_file) {
 
-    def config_dir = 'intro-to-packer'
-    def config_file = 'cocktails.pkr.hcl'
+  def packer = new packer.awsAMI()
 
     sh packer.init(config_dir)
 
     def output = sh(script: packer.build(config_dir, config_file), returnStdout: true)
 
     if (output.contains('amazon-ebs.cocktails: AMIs were created:')) {
+
         def lines = output.split('\n')
         def lastLine = lines[-1] 
         def amiId = lastLine.split(': ')[-1]
-        println amiId
+        def region = lastLine.split(': ')[0]
+
+        println "Region: ${region}"
+        println "Successfully created a new AMI with ID: ${amiId}."
+
+        return amiId
 
     } else {
       echo "${output}"
