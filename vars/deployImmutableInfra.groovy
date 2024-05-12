b34c0444915d26dfb094adc6bb11b9cd76e0d6c8
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2024-05-12 09:06:19 +0530

    Adding

--- vars/deployImmutableInfra.groovy
+++ vars/deployImmutableInfra.groovy
@@ -1,5 +1,5 @@
 def call() {
-    def launchTemplate = new LaunchTemplate()
+    def launchTemplate = new cloud.awscli.ec2.LaunchTemplate()
     sh launchTemplate.createVersion("lt-0be196a4ebe44a88b", "ami-0c1e8a55e8ea6420f")
 
   }
